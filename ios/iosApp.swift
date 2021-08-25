9e1e408963bdd03b8695d30a3dea237b0ae9350d
Berkant Çalıkuşu <berkantcalikusu@icloud.com>
2021-08-25 15:40:23 +0300

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by ilgazi on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
